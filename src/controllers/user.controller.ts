86a60bf5d4c4d51124a33b094df8041da356f8d0
Ravi <freelancework00700@gmail.com>
2024-02-02 15:38:18 +0530

    password field hashing

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { User, UserModel } from "../models/user.model";
 import { HttpStatus } from "../utils/httpStatus";
+import { hashPassword } from "../utils/helper";
 
 export class UserController extends HttpStatus {
 
   async signUp(req: Request, res: Response, next: NextFunction) {
     const httpStatus = new HttpStatus();
     try {
-        const user = new UserModel(req.body);
+        const requestBody: User = req.body;
+        requestBody.password = await hashPassword(requestBody.password);
+        
+        const user = new UserModel(requestBody);
         const result = await user.save();
 
         if(result){
